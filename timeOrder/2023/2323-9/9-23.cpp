d948a6cae60ffbf8c14583e6b5b724a762db5bb7
henke holm <121557616+waxeia@users.noreply.github.com>
2023-09-23 10:10:55 +0800

    Create 9-23.cpp

---
+++ timeOrder/2023/2323-9/9-23.cpp
@@ -0,0 +1,10 @@
+/*
+
+今日试跑代码：utaustinvilla3d文件夹，需要多加注意的文件如下（这周的主要任务）：
+* behaviors文件夹：naobehavior.cc; naobehavior.h; strategy.cc
+* worldmodel文件夹：worldmodel.cc; worldmodel.h; worldObject.h
+* (第三周的任务)headers文件夹：Field.h(场地信息); headers.cc; headers.h
+试跑流程：
+* $ rcsoccersim3d (robovize监视器和server都已经跑起来了)
+
+*/

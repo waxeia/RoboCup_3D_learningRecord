5375d57799608577c5c707a3a9f594f3f6840dd4
henke holm <121557616+waxeia@users.noreply.github.com>
2023-09-30 16:20:26 +0800

    Create one.cpp

---
+++ Mistake&Solution recording/one.cpp
@@ -0,0 +1,11 @@
+/*
+M: 
+waxeia@waxeia-virtual-machine:~/Myself/package/utaustinvilla3d$ cmake .
+-- No build type selected, default to Release
+-- Configuring done
+-- Generating done
+-- Build files have been written to: /home/waxeia/Myself/package/utaustinvilla3d
+S:
+多次构建，删去已存在的cmakecatch.txt文件，重新执行cmake .
+
+*/

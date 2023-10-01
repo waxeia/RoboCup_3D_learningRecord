fd49e32409e09b2074c9950f4588665f0614b675
henke holm <121557616+waxeia@users.noreply.github.com>
2023-10-01 16:43:50 +0800

    Update and rename (9-30)Bilibili's video learning.cpp to (9.30 - 10.1)Bilibili's video learning.cpp

---
+++ Systematic/(9.30 - 10.1)Bilibili's video learning.cpp
@@ -0,0 +1,30 @@
+/*
+about RoboCup3D competition
+* client-server mode（CS架构模式），client客户端，就是agent
+* agent: 智能体；球员
+* server: send messages(information of soccer field->编码->message->解码->information->计算；information of decision-making->编码->message to server->解码->simulation) 如此反复……
+* * 理论上如此反复一次是0.02s（人为规定），通信的格式涉及到原理的东西，暂时先不深究（机器人技术这门课会具体地讲解）
+* 计算；information of decision-making：this is what I should code
+*/
+
+
+/*
+* override /ˌoʊvərˈraɪd/ v.操控
+* beam /biːm/ n.（横）梁；here: 球员的上场位置
+* binder /ˈbaɪndər/ n.活页夹
+* factorization /ˌfæktərəˈzeɪʃən/ n.因式分解
+* factorize /ˈfæktəraɪz/ vt.因式分解
+* user-item n. 对象
+* sequential adj. 连续的；按顺序的
+* transition /trænˈzɪʃ(ə)n/ v.转换；迁移
+* bring tegether 集合；使……团结起来
+* personalized adj.个性化的
+* underlying /ˌʌndərˈlaɪɪŋ/ adj.根本的；表面下的
+* cube /kjuːb/ n.立方（体）
+* proposition /ˌprɑːpəˈzɪʃ(ə)n/ n.命题；主张；观点
+*/
+
+/*
+2023年9月30日
+爱一个不会有任何交集的人，我只能将其深深地压抑于心底，让这世界不会有第二个人知道这份爱的存在。今晚在学校露天的桌椅上学习，自己尚未察觉夜已渐凉，哥哥就已经发来降温的关切。最后就是，今晚暴食症Bulimia再次发作了，肚子真的好撑，做了一晚上的自我反思，对内心的根源还是有点儿模糊……
+*/
